42cbd8cdf3da98acc000ddeafd4d525fb81e62e4
Tomas Krizek <tkrizek@redhat.com>
2017-05-05 14:51:33 +0200

    machine_template clean up _ split files

--- tests/ansible/create_box.sh
+++ tests/ansible/create_box.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+chmod 600 keys/vagrant
+chmod 600 keys/freeipa_pr_ci_insecure
 ansible-playbook -vKi hosts_runner --vault-password-file vault-password --skip-tags xcleanup create_box.yml
