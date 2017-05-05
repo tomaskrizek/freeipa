93c38fab074d01798799e15d8526452a810ac948
Tomas Krizek <tkrizek@redhat.com>
2017-05-05 15:15:46 +0200

    create_box.sh: list packages and show prompt

--- tests/ansible/create_box.sh
+++ tests/ansible/create_box.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+# Fix private key permissions
 chmod 600 keys/vagrant
 chmod 600 keys/freeipa_pr_ci_insecure
+
+# Show contents of rpms directory
+ls rpms
+
+# Prompt for confirmation
+read -p "Proceed with these packages? [y/N] " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    # handle exits from shell or function but don't exit interactive shell
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+# Run playbook to create box
 ansible-playbook -vKi hosts_runner --vault-password-file vault-password --skip-tags xcleanup create_box.yml
+
